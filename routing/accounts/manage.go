cec675fd9353f619702764155f345faae07fe75d
Aliaksandr Karpovich <alexsure.k@gmail.com>
2019-02-20 21:53:08 +0300

    policy and account view changes

--- routing/accounts/manage.go
+++ routing/accounts/manage.go
@@ -8,9 +8,8 @@ import (
 )
 
 func (h *AccountHandler) View(w http.ResponseWriter, r *http.Request) {
-  var user models.User
-  s, _ := r.Context().Value("user").(*models.User)
-  log.Printf("LOGGED_IN_USER: %s", s)
+  user, _ := r.Context().Value("user").(*models.User)
+  log.Printf("LOGGED_IN_USER: %s", user)
 
   lib.SendJson(w, user, http.StatusOK)
 }
